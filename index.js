44321b498e71a01f0fe11bb35908e37b5dee10c7
Keoki <keoki.alan.mundon@gmail.com>
2021-11-17 19:20:11 -0800

    updated index.js

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
 
-// Select some elements...
+let dogImages = document.querySelectorAll('.dog-image')
+for (let i = 0; i < dogImages.length; i++){
+    dogImages[i].style.borderRadius = '50px'
+}
+
+let dogNames = document.querySelectorAll('.dog-name')
+for (let i = 0; i < dogNames.length; i++){
+    dogNames[i].style.textAlign = "left"
+}
+
+let footer = document.querySelector('.footer')
+footer.style.color = 'red'
